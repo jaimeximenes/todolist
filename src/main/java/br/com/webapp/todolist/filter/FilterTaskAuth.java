ab82c9b186ad888e496d20a363a6e7bc2df538f8
Jaime Ximenes <jaime.ximenes@gmail.com>
2023-10-15 21:41:07 -0300

    push API

---
+++ src/main/java/br/com/webapp/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,58 @@
+package br.com.webapp.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.webapp.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    IUserRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+
+        if (servletPath.startsWith("/tasks/")) {
+            var authorization = request.getHeader("Authorization");
+
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            byte[] authdecoded = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authdecoded);
+
+            String[] credentials = authString.split(":");
+            String userName = credentials[0];
+            String password = credentials[1];
+
+            var user = this.repository.findByUserName(userName);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
---
+++ src/main/java/br/com/webapp/todolist/user/UserController.java
@@ -0,0 +1,36 @@
+package br.com.webapp.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RestController()
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository repository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+
+        var user = this.repository.findByUserName(userModel.getUserName());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe!");
+        }
+
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.repository.save(userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+    }
+}
